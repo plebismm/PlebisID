3587be0327cdd976524bf52592c62bb77e5c77aa
Henrik Grotle <henrik.grotle@gmail.com>
2019-12-21 00:44:48 +0100

    added testweb

--- src/PlebisID.Server/Startup.cs
+++ src/PlebisID.Server/Startup.cs
@@ -26,7 +26,8 @@ namespace PlebisID.Server
             var builder = services.AddIdentityServer()
                 .AddInMemoryIdentityResources(Config.Ids)
                 .AddInMemoryApiResources(Config.Apis)
-                .AddInMemoryClients(Config.Clients);
+                .AddInMemoryClients(Config.Clients)
+                .AddTestUsers(TestUsers.Users);
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
