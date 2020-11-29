21b538ca537c813abcd64b17e8f8efce51f96fb9
Henrik Grotle <henrik.grotle@gmail.com>
2020-11-29 10:41:14 +0100

    reorganized files

---
+++ src/Plebis.Identity.TestClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Plebis.Identity.TestClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            DoStuff();
+            Console.ReadKey();
+        }
+
+        private static async Task DoStuff()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "Plebis.Identity-testapp",
+                ClientSecret = "secret",
+                Scope = "membership.read"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+        }
+    }
+}
---
+++ src/Plebis.Identity/MigrationsReadMe.txt
@@ -0,0 +1,12 @@
+﻿# Sqlite
+
+dotnet ef migrations add -c PlebisDbContext -n Identity --project ../Plebis.Identity.Migrations.Sqlite InitialPlebisDb
+
+dotnet ef migrations add -c ConfigurationDbContext -n IdentityServer.ConfigurationDb --project ../Plebis.Identity.Migrations.Sqlite InitialIdentityServer
+dotnet ef migrations add -c PersistedGrantDbContext -n IdentityServer.PersistedGrantDb --project ../Plebis.Identity.Migrations.Sqlite InitialIdentityServer
+
+# SqlServer
+
+dotnet ef migrations add -c PlebisDbContext -n Identity --project ../Plebis.Identity.Migrations.SqlServer Initial -- --storageEngine sqlserver
+dotnet ef migrations add -c ConfigurationDbContext -n IdentityServer.ConfigurationDb --project ../Plebis.Identity.Migrations.SqlServer InitialIdentityServer -- --storageEngine sqlserver
+dotnet ef migrations add -c PersistedGrantDbContext -n IdentityServer.PersistedGrantDb --project ../Plebis.Identity.Migrations.SqlServer InitialIdentityServer -- --storageEngine sqlserver
